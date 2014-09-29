ce49cb03ccc795fa3315c618403404485f536bd2
nahida1234 <nahida_036@yahoo.com>
2014-09-29 03:55:40 -0400

    Test hashtable

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+Hashtable test=new Hashtable();
+
+            test.Add("9","abc");
+            test.Add("8","def");
+
+            foreach (string b in test.Keys)
+            {
+              Console.WriteLine(b);
+                Console.ReadLine();
+            }
+            foreach (string b in test.Values)
+            {
+                Console.WriteLine(b);
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
